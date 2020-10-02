0202303ff2dd82b8fbc894d6db4aac469f6e081f
vellprolol <vellprolol@gmail.com>
2020-10-02 17:15:31 +0300

    Cats added and some of them are died

--- Loader.java
+++ Loader.java
@@ -3,8 +3,40 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+Cat Vasya = new Cat();
+        System.out.println("Вес Васи: " + Vasya.getWeight());
+        Vasya.feed (15 + Math.random() * 100);
+        System.out.println("Вес Васи после кормления: " + Vasya.getWeight());
 
-        System.out.println(cat.getStatus());
+
+Cat Murka = new Cat();
+        System.out.println("Вес Мурки: " + Murka.getWeight());
+        Murka.feed ( 15 + Math.random() * 100);
+        System.out.println("Вес Мурки после кормления: " + Murka.getWeight());
+
+
+Cat Sonya = new Cat();
+//Заставляем Соню умереть от переедания
+        System.out.println("Вес Сони: " + Sonya.getWeight());
+        while (Sonya.getStatus() != "Exploded") {
+            Sonya.feed(15 + Math.random() * 100);
+            System.out.println("Вес Сони после кормления: " + Sonya.getWeight());
+        }
+        System.out.println("Соня взорвалась от переедания.");
+
+
+Cat Musya = new Cat();
+//Заствялем Мусю замяукать себя до смерти
+        System.out.println("Вес Муси: " + Musya.getWeight());
+        while (Musya.getStatus() != "Dead") {
+            Musya.meow();
+            System.out.println("Вес Муси после мяукания: " + Musya.getWeight());
+        }
+        System.out.println("Муся умерла от истощения.");
+
+
+
+Cat Persik = new Cat();
+        System.out.println("Вес Персика: " + Persik.getWeight());
     }
 }
